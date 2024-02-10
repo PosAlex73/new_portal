a5632848279334e82812b7d9989a6bd13c4a5738
aposnov <a.posnov@fairtech.ru>
2024-02-11 01:09:40 +0400

    restore

--- src/Services/Practice/CodeClient.php
+++ src/Services/Practice/CodeClient.php
@@ -3,10 +3,21 @@
 namespace App\Services\Practice;
 
 use App\Dto\Practice\PracticeCodeDto;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class CodeClient
 {
     protected PracticeCodeDto $practiceCodeDto;
+
+    protected string $secret;
+
+    protected string $checkerUrl;
+
+    public function __construct(protected ParameterBagInterface $parameterBag)
+    {
+        $this->secret = $this->parameterBag->get('lalalala');
+        $this->checkerUrl = $this->parameterBag->get('checker_url');
+    }
 
     public function setCodeDto(PracticeCodeDto $practiceCodeDto)
     {
