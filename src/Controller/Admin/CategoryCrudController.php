a1c794897c957d2cdcfb76e55e1980f69e758d22
aposnov <a.posnov@fairtech.ru>
2024-10-04 22:52:39 +0400

    продолжаю улучшать админку

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -4,13 +4,17 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Enums\CommonStatus;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 
 class CategoryCrudController extends AbstractCrudController
 {
@@ -25,10 +29,7 @@ class CategoryCrudController extends AbstractCrudController
             IdField::new('id')->setDisabled(),
             TextField::new('title'),
             TextEditorField::new('text'),
-            ChoiceField::new('status')->setChoices([
-                'Активно' => CommonStatus::ACTIVE->value,
-                'Отключено' => CommonStatus::DISABLED->value,
-            ]),
+            ChoiceField::new('status')->setChoices($this->getStatusChoices()),
         ];
 
         if ($pageName === Crud::PAGE_INDEX) {
@@ -37,5 +38,35 @@ class CategoryCrudController extends AbstractCrudController
         }
 
         return $fields;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        $filters->add(ChoiceFilter::new('status')->setChoices($this->getStatusChoices()));
+        $filters->add('created');
+        $filters->add('updated');
+
+        return $filters;
+    }
+
+    private function getStatusChoices(): array
+    {
+        return [
+            'Активно' => CommonStatus::ACTIVE->value,
+            'Отключено' => CommonStatus::DISABLED->value,
+        ];
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setDefaultSort(['updated' => 'DESC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        $actions->remove(Crud::PAGE_INDEX, Action::DELETE);
+        $actions->remove(Crud::PAGE_DETAIL, Action::DELETE);
+
+        return $actions;
     }
 }
--- src/Controller/Admin/CourseTagCrudController.php
+++ src/Controller/Admin/CourseTagCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\CourseTag;
 use App\Enums\CommonStatus;
+use App\Enums\System\TagColors;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
@@ -11,6 +14,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
 
 class CourseTagCrudController extends AbstractCrudController
 {
@@ -19,19 +23,55 @@ class CourseTagCrudController extends AbstractCrudController
         return CourseTag::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id')->setDisabled(),
             TextField::new('title'),
-            ChoiceField::new('status')->setChoices([
-                'Активно' => CommonStatus::ACTIVE->value,
-                'Отключено' => CommonStatus::DISABLED->value
-            ]),
+            ChoiceField::new('status')->setChoices($this->getStatusChoices()),
+            ChoiceField::new('color')->setChoices($this->getColorChoices()),
             AssociationField::new('courses')->autocomplete(),
             DateTimeField::new('created')->setDisabled(),
             DateTimeField::new('updated')->setDisabled()
         ];
+    }
+
+    private function getStatusChoices(): array
+    {
+        return [
+            'Активно' => CommonStatus::ACTIVE->value,
+            'Отключено' => CommonStatus::DISABLED->value
+        ];
+    }
+
+    private function getColorChoices(): array
+    {
+        return [
+            'Primary' => TagColors::PRIMARY->value,
+            'Secondary' => TagColors::SECONDARY->value,
+            'Success' => TagColors::SUCCESS->value,
+            'Danger' => TagColors::DANGER->value,
+            'Dark' => TagColors::DARK->value,
+            'Warning' => TagColors::WARNING->value,
+            'Light' => TagColors::WARNING->value,
+            'Info' => TagColors::INFO->value
+        ];
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        $filters->add('created');
+        $filters->add('updated');
+        $filters->add(ChoiceFilter::new('color')->setChoices($this->getColorChoices()));
+        $filters->add(ChoiceFilter::new('status')->setChoices($this->getColorChoices()));
+
+        return $filters;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        $crud->setDefaultSort(['id' => 'DESC']);
+
+        return $crud;
     }
 }
