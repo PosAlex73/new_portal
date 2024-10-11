e094fbe1068ccaa7b0f058b6ca8e8b087190519d
aposnov <a.posnov@fairtech.ru>
2024-10-12 01:59:59 +0400

    добавил иконки верхнему меню

--- src/Services/Menu/AdminMainMenu.php
+++ src/Services/Menu/AdminMainMenu.php
@@ -13,9 +13,9 @@ class AdminMainMenu
     public function getMainMenu()
     {
         return [
-            new MenuElement('Курсы', $this->generator->generate('courses_list'), 'mdi-concourse-ci'),
-            new MenuElement('Новости', $this->generator->generate('news_list'), 'mdi-forum-outline'),
-            new MenuElement('Статьи', $this->generator->generate('blog_list'), 'mdi-blogger'),
+            new MenuElement('Курсы', $this->generator->generate('courses_list'), 'fa fa-solid fa-graduation-cap'),
+            new MenuElement('Новости', $this->generator->generate('news_list'), 'fa fa-star'),
+            new MenuElement('Статьи', $this->generator->generate('blog_list'), 'fa fa-crosshairs'),
         ];
     }
 }
