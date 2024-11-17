ff2e8f16e7124e6957967fed11686cbc0305aad8
aposnov <a.posnov@fairtech.ru>
2024-11-17 22:47:05 +0400

    test

--- src/Controller/Front/SearchController.php
+++ src/Controller/Front/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Front;
 
+use App\Enums\Http\HttpRequest;
+use App\Services\Menu\BreadCrumbsBuilder;
 use App\Services\Search\SearchService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,20 +12,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
-    public function __construct(protected SearchService $searchService)
+    public function __construct(
+        protected SearchService $searchService,
+        protected BreadCrumbsBuilder $breadCrumbsBuilder
+    )
     {
     }
 
     #[Route('/search', name: 'search')]
     public function index(Request $request): Response
     {
-        $text = $request->get('search');
-        $search = $this->searchService->search($text);
+        $this->initBreadCrumbs();
+        if ($request->getMethod() === HttpRequest::POST->value) {
+            $text = $request->get('search');
+            $search = $this->searchService->search($text);
+
+            return $this->render('front/search/index.html.twig', [
+                'courses' => $search['courses'],
+                'articles' => $search['articles'],
+                'news' => $search['news'],
+                'newSearch' => false
+            ]);
+        }
 
         return $this->render('front/search/index.html.twig', [
-            'courses' => $search['courses'],
-            'articles' => $search['articles'],
-            'news' => $search['news']
+            'newSearch' => true
         ]);
+    }
+
+    private function initBreadCrumbs()
+    {
+        $this->breadCrumbsBuilder->addIndexRoute();
+        $this->breadCrumbsBuilder->addBreadCrumbs('Поиск', $this->generateUrl('search'));
     }
 }
